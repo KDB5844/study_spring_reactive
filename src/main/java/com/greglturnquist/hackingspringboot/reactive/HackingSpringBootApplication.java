5e346153facad80357f34cf5dc994eec5d81f26e
KDB5844 <sbjo7713@gmail.com>
2023-03-02 02:48:21 +0900

    간단한 비동기 테스트

--- src/main/java/com/greglturnquist/hackingspringboot/reactive/HackingSpringBootApplication.java
+++ src/main/java/com/greglturnquist/hackingspringboot/reactive/HackingSpringBootApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class HackingSpringBootApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(HackingSpringBootApplication.class, args);
 	}
